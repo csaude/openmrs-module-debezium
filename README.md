607a0c83ce0464ee2ed233582891b27f43488331
wluyima <wyclif@openmrs.org>
2021-06-30 20:22:24 -0500

    EC-74 - Added activator, config.xml, renamed artifact id and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# debezium-mysql-connector
-Provides a debezium database connector
+# Debezium Module
+Provides a mechanism to monitor changes in an OpenMRS database using an embedded debezium engine and notifies any 
+configured listeners. 
+
+Currently, the only monitored operations are row level inserts, updates and deletes.
---
+++ api/src/main/java/org/openmrs/module/debezium/DebeziumActivator.java
@@ -0,0 +1,47 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.debezium;
+
+import org.openmrs.module.BaseModuleActivator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DebeziumActivator extends BaseModuleActivator {
+	
+	private static final Logger log = LoggerFactory.getLogger(DebeziumActivator.class);
+	
+	/**
+	 * @see BaseModuleActivator#started()
+	 */
+	@Override
+	public void started() {
+		log.info("Debezium module started");
+	}
+	
+	/**
+	 * @see BaseModuleActivator#stopped()
+	 */
+	@Override
+	public void stopped() {
+		log.info("Debezium module stopped");
+	}
+	
+	/**
+	 * @see BaseModuleActivator#willStop()
+	 */
+	@Override
+	public void willStop() {
+		if (log.isInfoEnabled()) {
+			log.info("Stopping debezium engine");
+		}
+		//TODO Stop debezium engine
+	}
+	
+}
