d695a640b9f7b077870391e96aca48eb3d2dc6a4
wluyima <wyclif@openmrs.org>
2024-07-11 19:29:50 +0300

    Added utility methods to set values and invokde methods via reflection

---
+++ api/src/test/java/org/openmrs/module/debezium/UtilsTest.java
@@ -0,0 +1,42 @@
+package org.openmrs.module.debezium;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.openmrs.ConceptName;
+import org.openmrs.Person;
+import org.openmrs.api.impl.ConceptServiceImpl;
+
+public class UtilsTest {
+	
+	@Test
+	public void setFieldValue_shouldSetTheFieldValue() throws Exception {
+		Person person = new Person();
+		Field field = Person.class.getDeclaredField("gender");
+		assertFalse(field.canAccess(person));
+		final String gender = "M";
+		assertNull(person.getGender());
+		Utils.setFieldValue(person, field, gender);
+		assertEquals(gender, person.getGender());
+		assertFalse(field.canAccess(person));
+	}
+	
+	@Test
+	public void invokeMethod_shouldInvokeTheMethod() throws Exception {
+		ConceptServiceImpl service = new ConceptServiceImpl();
+		Method method = ConceptServiceImpl.class.getDeclaredMethod("cloneConceptName", ConceptName.class);
+		assertFalse(method.canAccess(service));
+		
+		ConceptName c = (ConceptName) Utils.invokeMethod(service, method, new ConceptName());
+		
+		assertNotNull(c);
+		assertFalse(method.canAccess(service));
+	}
+	
+}
