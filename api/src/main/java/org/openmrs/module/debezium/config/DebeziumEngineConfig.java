f88d09b1eae53207f71bd1880934d06510a6f6d8
Daniel Chirinda <chirindzadaniel@gmail.com>
2025-03-28 11:05:31 +0200

    Refactor config and debezium Event

--- api/src/main/java/org/openmrs/module/debezium/config/DebeziumEngineConfig.java
+++ api/src/main/java/org/openmrs/module/debezium/config/DebeziumEngineConfig.java
@@ -60,38 +60,5 @@ public interface DebeziumEngineConfig {
 	default Set<String> getTablesToExclude() {
 		return null;
 	}
-	
-	class DebeziumActivator extends BaseModuleActivator {
-		
-		private static final Logger log = LoggerFactory.getLogger(DebeziumActivator.class);
-		
-		/**
-		 * @see org.openmrs.module.BaseModuleActivator#started()
-		 */
-		@Override
-		public void started() {
-			log.info("Debezium module started, starting OpenMRS debezium engine");
-			
-			DebeziumEngineManager.start();
-		}
-		
-		/**
-		 * @see org.openmrs.module.BaseModuleActivator#stopped()
-		 */
-		@Override
-		public void stopped() {
-			log.info("Debezium module stopped");
-		}
-		
-		/**
-		 * @see org.openmrs.module.BaseModuleActivator#willStop()
-		 */
-		@Override
-		public void willStop() {
-			log.info("Stopping OpenMRS debezium engine before debezium module is stopped");
-			
-			DebeziumEngineManager.stop();
-		}
-		
-	}
+
 }
